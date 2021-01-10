4bcd3e7d1c97f0f82c6cef62e37fcea9229cf283
Piotr Orlowski <p.orlowski1@gmail.com>
2021-01-10 16:39:14 +0100

    develop: remove console.log

--- src/store/users/utils.ts
+++ src/store/users/utils.ts
@@ -1,4 +1,3 @@
 export default function checkIfUserAlreadyExists(users: Array<object>, userEmail: string): boolean {
-  console.log(users.some((user: Record<string, any>): boolean => user.email === userEmail));
   return users.some((user: Record<string, any>): boolean => user.email === userEmail);
 }
